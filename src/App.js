0f37d11bccac1fee0c3cadbfbc38debf5fcd04f5
M3LKY <melquisedecgonzalez74@gmail.com>
2023-06-13 16:35:55 -0400

    Merge remote changes

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+import AppContext from "./Context/context";
+function App() {
+  return /*#__PURE__*/React.createElement(BrowserRouter, null, /*#__PURE__*/React.createElement(AppContext, null, /*#__PURE__*/React.createElement(Header, null), /*#__PURE__*/React.createElement(Routes, null, /*#__PURE__*/React.createElement(Route, {
+    path: "/tech-store",
+    element: /*#__PURE__*/React.createElement(Home, null)
+  }), /*#__PURE__*/React.createElement(Route, {
+    path: "/tech-store/category/:id",
+    element: /*#__PURE__*/React.createElement(Category, null)
+  }), /*#__PURE__*/React.createElement(Route, {
+    path: "/tech-store/product/:id",
+    element: /*#__PURE__*/React.createElement(SingleProduct, null)
+  })), /*#__PURE__*/React.createElement(Newsletter, null), /*#__PURE__*/React.createElement(Footer, null)));
+}
+export default App;
